bfe802d35715dd0a9329896d2f5e7dc25281bca1
farouk-hajjej <74997699+farouk-hajjej@users.noreply.github.com>
2022-10-26 04:26:28 +0200

    farouk-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import {RouterModule} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
